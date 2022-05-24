345ad8ed0b06bc733d4218f9e301f4f0095a6e24
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-24 09:19:41 -0300

    Initial commit

---
+++ Assets/EggMaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggMaker : MonoBehaviour
+{
+    public GameObject eggPrefab;
+    public GameObject eggSpawnPoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(eggPrefab);
+            clon.transform.position = eggSpawnPoint.transform.position;
+        }
+    }
+}
